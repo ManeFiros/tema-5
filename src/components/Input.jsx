41f4a487c83b24909225427393801b6fed520f61
Mané Firós <sendmyasith@gmail.com>
2021-03-31 22:19:28 +0200

    third

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ComponenteEventos from './ComponenteEventos'
+
+class Input extends Component{
+    WebGL2RenderingContext(){
+        <div>
+            <input OnKeyPress="manejarPulsacionesTeclado" onKeyDown="manejarDown" />
+        </div>
+    };
+}
+
+export default Input;
