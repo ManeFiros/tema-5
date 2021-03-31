2d888c75dcb193e82d47988b9c36d2d13820446c
Mané Firós <sendmyasith@gmail.com>
2021-03-31 22:10:00 +0200

    second

--- src/components/ComponenteEventos.jsx
+++ src/components/ComponenteEventos.jsx
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 function ComponenteEventos (){
     const [texto, setTexto] = useState('');
 
+    //OnKeyPress
     const manejarPulsacionesTeclado = (event) =>{
         setTexto(texto+event.charCode);
+    }
+
+    //onKeyDown
+    const manejarDown = (event) =>{
+        setTexto(texto+String.fromCharCode(8)+event.charCode);
     }
 }
 
